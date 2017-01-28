3492fdb69c8e3dd3b20069fffc39ae85ba766cd1
dejun <pudejun15605193770@gmail.com>
2017-01-28 02:13:46 -0500

    angular

---
+++ home/home.controller.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    'use strict';
+   
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+        var list=[];
+		var json={
+		    "recipient":"Arun",
+            "sender":"Nanda",
+            "title":"This is a sample message to Arun.",
+            "description":"This is a sample description to the message which has the above title"
+		}
+		
+    HomeController.$inject = ['UserService', '$rootScope','$scope'];
+    function HomeController(UserService, $rootScope,$scope) {
+        var vm = this;
+        vm.user = null;
+        vm.allUsers = [];
+        vm.deleteUser = deleteUser;
+        initController();
+
+        function initController() {
+            loadCurrentUser();
+            loadAllUsers();
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+					
+                    vm.user = user;
+                });
+        }
+
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
+
+        function deleteUser(id) {
+            UserService.Delete(id)
+            .then(function () {
+                loadAllUsers();
+            });
+        }
+
+		$scope.say=function(u){
+		  json.recipient=vm.user.username;	 
+		  json.sender=u.user.username;
+		  json.title=$scope.title;
+		  json.description=$scope.description;
+		  list.push(json);
+	       if(localStorage.getItem("chat")===null){
+		  localStorage.setItem("chat",JSON.stringify(list));
+		  }
+		  else{
+		  var l=JSON.parse(localStorage.getItem("chat"));
+		  l.push(json);
+		  localStorage.setItem("chat",JSON.stringify(l));
+		}
+		}
+	}
+
+})();
---
+++ login/login.controller.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService','$rootScope'];
+    function LoginController($location, AuthenticationService, FlashService,$rootScope) {
+        var vm = this;
+        vm.login = login;
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+					$rootScope.uname=vm.username;
+					$rootScope.pword=vm.password;
+                    $location.path('/');
+                } else {
+					alert("failed login");
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+                }
+            });
+        };
+    }
+})();
